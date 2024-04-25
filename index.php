1a82e50fb05914b6ea9c3818f42b036a6baee77e
JC <676half@gmail.com>
2024-04-25 11:18:10 -0400

    Second Change

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
-print("This is my First Php Code for this Azure Project");
+print("This is my Modified  Php Code for this Azure Project");
 print("<br>");
-print("I am version 1 of php code in azure");
+print("I am version 2 of php code in azure");
+print("<br>");
+print("This is my First Updated  Php Code for this Azure Project");
 
 ?>
 
